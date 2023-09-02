11a2b52a7e3c1506908f86d4a050138fe21efe71
Leaovo-man2 <samsayamy@outlook.com>
2023-09-02 14:10:06 +0800

    add

--- 自制项目/查询，录入分数.py
+++ 自制项目/查询，录入分数.py
@@ -1,15 +1,15 @@
 k = input('您要进行什么操作？ （查询），（录入）：')
 if k == '查询':
-    n = 'Mickey','Bob','Jhon'
-    d = {'Mickey': 95, 'Bob': 100, 'Jhon': 90 }
+    n = 'Mickey', 'Bob', 'Jon'
+    d = {'Mickey': 95, 'Bob': 100, 'Jon': 90}
     print(n)
-    print(d[input('学生姓名 ')],'分')
+    print(d[input('学生姓名 ')], '分')
 if k == '录入':
-    na = ['Mickey','Bob','Jhon']
+    na = ['Mickey', 'Bob', 'Jon']
     nam = input('新学生名字？ ')
     na.append(nam)
     f = input('分数？')
-    d = {'Mickey': 95, 'Bob': 100, 'Jhon': 90, nam : f}
+    d = {'Mickey': 95, 'Bob': 100, 'Jon': 90, nam: f}
     print('添加成功！')
     print(d)
-    print(d[input('学生姓名 ')],'分')
+    print(d[input('学生姓名 ')], '分')
